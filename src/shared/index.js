e93eb92d7dc987f57ac224816f2112da67249246
Debojyoti Ghosh <debojyoti.ghosh@housing.com>
2021-06-03 20:06:16 +0530

    Comments removed.

--- src/shared/index.js
+++ src/shared/index.js
@@ -3,7 +3,6 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
-// import { Global, css } from '@emotion/react'
 
 import routes from './routes'
 
@@ -18,18 +17,6 @@ const App = () => {
           key={index}
         />
       ))}
-      {/* <Global
-        styles={css`
-          * {
-            margin: 0;
-            padding: 0;
-          }
-          html, body {
-            width: 100%;
-            height: 100%;
-          }
-        `}
-      /> */}
     </Switch>
   )
 }
