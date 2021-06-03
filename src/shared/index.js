0c6cf811455ab62670d1004d4abcdd5a8293dde8
Debojyoti Ghosh <debojyoti.ghosh@housing.com>
2021-06-03 20:05:57 +0530

    Global styles.

--- src/shared/index.js
+++ src/shared/index.js
@@ -3,6 +3,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+// import { Global, css } from '@emotion/react'
 
 import routes from './routes'
 
@@ -17,6 +18,18 @@ const App = () => {
           key={index}
         />
       ))}
+      {/* <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+          }
+          html, body {
+            width: 100%;
+            height: 100%;
+          }
+        `}
+      /> */}
     </Switch>
   )
 }
